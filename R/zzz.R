be59b9ae91050be2a26405cf9ea77c611439ccbd
estfernan <91330857+estfernan@users.noreply.github.com>
2021-12-06 16:21:23 -0500

    Loading message

---
+++ R/zzz.R
@@ -0,0 +1,36 @@
+##
+## R package boost by Esteban Fernández, Xi Jiang, Suhana Bedi, and Qiwei Li
+## Copyright (C) 2021
+##
+## This file is part of the R package boost.
+##
+## The R package boost is free software: You can redistribute it and/or
+## modify it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or any later
+## version (at your option). See the GNU General Public License at
+## <https://www.gnu.org/licenses/> for details.
+##
+## The R package boost is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+##
+
+## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+## This loading message should be temporary until SPARK can be installed
+## through CRAN without any issues. Currently, SPARK can only be installed
+## through GitHub.
+## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+.onLoad <- function(libname, pkgname)
+{
+  packageStartupMessage(
+    message("boost: Bayesian modeling of spatial transcriptomics data\n",
+            "\n",
+            "To use the SPARK model, please install the source package by:\n",
+            "\n",
+            "  #> install.packages('devtools')\n",
+            "  #> devtools::install_github('xzhoulab/SPARK')\n",
+            "\n",
+            "More information about the package can be found in <https://github.com/estfernan/boost>.")
+  )
+}
