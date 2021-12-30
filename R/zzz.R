7ac0547c5869768615f08df707f95f7c913d2003
estfernan <91330857+estfernan@users.noreply.github.com>
2021-12-30 11:33:02 -0600

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -18,24 +18,27 @@
 ## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ## This loading message should be temporary until SPARK can be installed
 ## through CRAN without any issues. Currently, SPARK can only be installed
-## through GitHub.
+## through GitHub. Although, SpatialDE needs to be installed through
+## Bioconductor, so the message might need to stay.
 ## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-.onLoad <- function(libname, pkgname)
+.onAttach <- function(libname, pkgname)
 {
   packageStartupMessage(
-    message("boost: Bayesian modeling of spatial transcriptomics data\n",
-            "\n",
-            "To use the SPARK model, please install the source package by:\n",
-            "\n",
-            "  #> install.packages('devtools')\n",
-            "  #> devtools::install_github('xzhoulab/SPARK')\n",
-            "\n",
-            "To use the SpatialDE model, please install the source package by:\n",
-            "\n",
-            "  #> install.packages('BiocManager')\n",
-            "  #> BiocManager::install('spatialDE')\n",
-            "\n",
-            "More information about the package can be found in <https://github.com/estfernan/boost>.")
+    paste0(
+      "boost: Bayesian modeling of spatial transcriptomics data\n",
+      "\n",
+      "To use the SPARK model, please install the source package by:\n",
+      "\n",
+      "  #> install.packages('devtools')\n",
+      "  #> devtools::install_github('xzhoulab/SPARK')\n",
+      "\n",
+      "To use the SpatialDE model, please install the source package by:\n",
+      "\n",
+      "  #> install.packages('BiocManager')\n",
+      "  #> BiocManager::install('spatialDE')\n",
+      "\n",
+      "More information about the package can be found in <https://github.com/estfernan/boost>."
+    )
   )
 }
