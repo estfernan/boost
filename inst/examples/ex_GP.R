dbdf9180050c04ae053c2223302d01da1b1a2bf2
estfernan <91330857+estfernan@users.noreply.github.com>
2022-01-10 13:57:27 -0600

    Implemented BOOST-GP example

---
+++ inst/examples/ex_GP.R
@@ -0,0 +1,14 @@
+\dontrun{
+library(boost)
+
+## load sample dataset
+data(mob)
+
+## extract a sample gene and get size factor
+g <- mob[, "Apoe"]
+s <- get.size.factor(mob, estimation.method = "TSS")
+
+## fit the model
+res <- BOOST.GP(g, mob.spots, size.factor = s, gene.name = "Apoe", n.iter = 200)
+print(res)
+}
